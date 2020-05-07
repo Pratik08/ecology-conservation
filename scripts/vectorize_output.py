5da709e7c976104a1e7af924fe381e24a2416de0
Pratik Dubal <Pratik08@users.noreply.github.com>
2020-05-07 11:10:38 -0400

    Update vectorize_output.py

--- scripts/vectorize_output.py
+++ scripts/vectorize_output.py
@@ -4,58 +4,49 @@ import pandas as pd
 import numpy as np
 
 from sklearn.preprocessing import MultiLabelBinarizer
-
 mlb = MultiLabelBinarizer()
 
-data = pd.read_csv('../Data/AllData - AllData.csv')
-data = data.drop([ele for ele in data.columns.to_list()
-                 if ele in ['X', 'Random Sort', 'Unnamed: 0']], axis=1)
+'''
+Path to data files
+'''
+raw_data = '/home/psd2120/research/Data/AllData - AllData.csv'
+preprocess_phrases = '/home/psd2120/research/Data/Preprocessing/Preprocessing_Phrases_v3.csv'
+master_file = '/home/psd2120/research/Data/Output/Master Output/v9/master_output_v9_temp.csv'
+output_file = '/home/psd2120/research/Data/Output/Master Output/v9/master_output_data_analysis_v9.csv'
+
+'''
+Preprocess
+'''
+data = pd.read_csv(raw_data)
+preproc_ph = pd.read_csv(preprocess_phrases)
+preproc_phrases = list(preproc_ph.Phrases)
+data = data.drop([ele for ele in data.columns.to_list() if ele in ['X', 'Random Sort', 'Unnamed: 0']], axis=1)
 data = data.dropna(subset=['Description'])
-data = data[data.Description != 'For general operating support']
-data = data[data.Description != 'For General Operating Support']
-data = data[data.Description != 'For operating support']
-data = data[data.Description != 'Charitable']
-data = data[data.Description != "For the organization's charitable use"]
-data = data[data.Description != 'For capital support']
-data = data[data.Description != 'For annual support']
-data = data[data.Description != 'For annual giving']
-data = data[data.Description != 'For general support']
-data = data[data.Description != 'OPERATING']
-data = data[data.Description != 'For operating support']
-data = data[data.Description != 'Operating']
-data = data[data.Description != 'OPERATIONAL SUPPORT']
-data = data[data.Description != 'Operational support']
-data = data[data.Description != 'General Operating Support']
-data = data[data.Description != 'General purposes']
-data = data[data.Description != 'General purpose']
-data = data[data.Description != 'General']
-data = data[data.Description != 'Support']
-data = data[data.Description.str.lower() != 'general support']
-data = data[data.Description.str.lower() != 'for general support']
-data = data[data.Description.str.lower() != 'general operating']
-data = data[data.Description.str.lower() != 'for general operating']
-data = data[data.Description.str.lower() != 'for capital']
-data = data[data.Description.str.lower() != 'general operating support']
-data = data[data.Description.str.lower() != 'for program support']
+
+for phrase in preproc_phrases:
+    data = data[data.Description.str.lower() != phrase]
+
 data = data.reset_index(drop=True)
 
-data_class = pd.read_csv('../Data/Output/master_output_v2.csv')
+data_class = pd.read_csv(master_file)
 data_class = data_class.fillna('-')
 
-pbar = tqdm.tqdm(total=61961)
-for idx in range(61961):
+pbar = tqdm.tqdm(total=len(data))
+for idx in range(len(data)):
     for col in data_class.columns[1:]:
         if type(data_class[col][idx]) == str:
             data_class[col][idx] = data_class[col][idx].split('|')
     pbar.update(1)
 pbar.close()
 
-df_final = pd.concat([data, data_class[[' Keywords', ' Categories', 'Locations', 'Bio_Keywords', 'Bioregions']]], axis=1)
+'''
+Generate output file
+'''
+df_final = pd.concat([data, data_class[['Keywords','Filtered_Categories', 'Bio_keywords','Bio_regions']]], axis=1)
+df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Filtered_Categories')),columns=['cat_' + x for x in mlb.classes_],index=df_final.index))
+df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Bio_keywords')),columns=['bio_key_' + x for x in mlb.classes_],index=df_final.index))
+df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Bio_regions')),columns=['bio_reg_' + x for x in mlb.classes_],index=df_final.index))
+df_final = df_final.drop(['cat_-', 'bio_key_-', 'bio_reg_-'], axis=1)
+df_final.to_csv(output_file, index=False)
 
-df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop(' Categories')),columns=['cat_' + x for x in mlb.classes_],index=df_final.index))
-df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Locations')),columns=['loc_' + x for x in mlb.classes_],index=df_final.index))
-df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Bio_Keywords')),columns=['bio_key_' + x for x in mlb.classes_],index=df_final.index))
-df_final = df_final.join(pd.DataFrame(mlb.fit_transform(df_final.pop('Bioregions')),columns=['bio_reg_' + x for x in mlb.classes_],index=df_final.index))
-
-df_final = df_final.drop(['cat_-', 'loc_-', 'bio_key_', 'bio_reg_'])
-df_final.to_csv('../Data/Output/master_output_data_analysis_v1.csv', index=False)
+print('Completed')
